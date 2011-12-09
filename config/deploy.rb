c429f2c23769a97747a5814603f9bc2373d5b75c
Craig Rindt <crindt@translab.its.uci.edu>
2011-12-09 13:30:20 -0800

    More configuration tweaks for capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,24 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "ctmlabs"
+set :repository,  "git@parsons.its.uci.edu:/home/crindt/ctmlabs-nesta.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_to, "/var/lib/nesta/#{application}"
+
+role :web, "parsons.its.uci.edu"                          # Your HTTP server, Apache/etc
+role :app, "parsons.its.uci.edu"                          # This may be the same as your `Web` server
+role :db,  "parsons.its.uci.edu", :primary => true # This is where Rails migrations will run
+role :db,  "parsons.its.uci.edu"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
