dae51fa0faab02093591768ad0cb34b5856522d0
Craig Rindt <crindt@translab.its.uci.edu>
2012-03-22 11:02:08 -0700

    Added files that should have been included in commit:5a8b596e0972b575fe2475a4ce34927969868279.  Refs #689

---
+++ themes/ctmlabs/public/ctmlabs/js/ctmlabs-banner-fixed.js
@@ -0,0 +1,73 @@
+(function(){
+    ctmlabs = {version:"0.0.1"};
+
+    window.onload = function(){
+        var menuStr ='  <nav id="ctmlabs-nav" class="navbar navbar-fixed-top"> \
+    <div class="navbar-inner"> \
+      <div class="container-fluid"> \
+        <!-- .btn-navbar is used as the toggle for collapsed navbar content --> \
+        <a class="btn btn-navbar" data-target=".nav-collapse" data-toggle="collapse"> \
+          <span class="icon-bar"></span> \
+          <span class="icon-bar"></span> \
+          <span class="icon-bar"></span> \
+        </a> \
+        <a alt="CTMLabs" class="ir brand" href="http://wwwdev.ctmlabs.net/ctmlabs/" id="ctmllink" title="CTMLabs home"></a> \
+        <div class="brand"> \
+          CTMLabs\
+        </div> \
+        <div class="nav-collapse"> \
+          <ul class="nav"> \
+            <li class="dropdown"> \
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#"> \
+                Applications\
+                <b class="caret"></b> \
+              </a> \
+              <ul class="dropdown-menu"> \
+                <li> \
+                  <a href="https://tmcpe.ctmlabs.net/tmcpe-1.3">TMC Performance Evaulation</a> \
+                </li> \
+                <li> \
+                  <a href="http://128.200.36.104:8080/SATMSWeb">Ramp Meter Evaluation Platform</a> \
+                </li> \
+                <li> \
+                  <a href="http://moon.its.uci.edu/inside">INSIDE Laboratory</a> \
+                </li> \
+                <li> \
+                  <a href="http://safety.ctmlabs.net/safetynet">Safety</a> \
+                </li> \
+              </ul> \
+            </li> \
+          </ul> \
+          <ul class="nav pull-right"> \
+            <li> \
+              <a href="http://tracker.ctmlabs.net/projects/tb/issues/new" title="Report a problem with this website">Report Problem</a> \
+            </li> \
+            <li class="dropdown"> \
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#"> \
+                Help\
+                <b class="caret"></b> \
+              </a> \
+              <ul class="dropdown-menu"> \
+                <li> \
+                  <a href="http://wwwdev.ctmlabs.net/ctmlabs/help" title="Site help">Site Docs</a> \
+                </li> \
+                <li> \
+                  <a href="http://wwwdev.ctmlabs.net/ctmlabs/about/people">Contact</a> \
+                </li> \
+              </ul> \
+            </li> \
+          </ul> \
+        </div> \
+      </div> \
+    </div> \
+  </nav> \
+';
+        $("body").prepend(menuStr);
+        $("body").css('padding-top','60px');
+
+        $(".navbar-fixed-top [title]").tooltip({placement:"bottom"});
+        
+    };
+        
+})()
+
