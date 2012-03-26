aec3e68670f5750ccdd48d4e9da4476fddb497d4
Craig Rindt <crindt@translab.its.uci.edu>
2012-03-26 16:43:13 -0700

    Added namespaced deploy tasks for vlad

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,32 @@
 require "bundler/vlad"
 
-set :application, "ctmlabs-nesta"
-set :repository,  "git@metis.its.uci.edu:ctmlabs-nesta"
-set :domain, "wwwdev.ctmlabs.net"
-set :deploy_to, "/Web/apps/#{application}"
-set :revision, "origin/develop"
-set :bundle_cmd, [ "source /usr/local/rvm/scripts/rvm",
-                    "bundle"
-                  ].join(" && ")
+namespace :vlad do
+  namespace :development do
+    set :application, "ctmlabs-nesta"
+    set :repository,  "git@metis.its.uci.edu:ctmlabs-nesta"
+    set :domain, "wwwdev.ctmlabs.net"
+    set :deploy_to, "/Web/apps/#{application}"
+    set :revision, "origin/develop"
+    set :bundle_cmd, [ "source /usr/local/rvm/scripts/rvm",
+                       "bundle"
+                     ].join(" && ")
+    task :deploy => %w[
+      vlad:update vlad:bundle:install vlad:start_app vlad:cleanup
+    ]
+  end
+  
+  namespace :production do
+    set :application, "ctmlabs-nesta"
+    set :repository,  "git@metis.its.uci.edu:ctmlabs-nesta"
+    set :domain, "www.ctmlabs.net"
+    set :deploy_to, "/Web/apps/#{application}"
+    set :revision, "origin/master"
+    set :bundle_cmd, [ "source /usr/local/rvm/scripts/rvm",
+                       "bundle"
+                     ].join(" && ")
+    task :deploy => %w[
+      vlad:update vlad:bundle:install vlad:start_app vlad:cleanup
+    ]
+  end
+end
 
-task "vlad:deploy" => %w[
-  vlad:update vlad:bundle:install vlad:start_app vlad:cleanup
-]
